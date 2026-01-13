4dca6a4e862a2ce99ccc437b767846db3a0198df
Rohit Kumar Chaudhary <engrohitv@gmail.com>
2026-01-13 11:13:20 +0530

    sitemap update

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'delhizones-v4';
+const urlsToCache = [
+  '/',
+  '/includes/head.html',
+  '/includes/navbar.html',
+  '/includes/footer.html',
+  '/contact.html',
+  '/logo.svg'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
