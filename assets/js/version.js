c4c52fdade0d197ade5ffa1b16933b77116245f2
Rohit Kumar Chaudhary <engrohitv@gmail.com>
2026-01-24 10:50:06 +0530

    result update

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,2 +1,2 @@
 // Central version management - update this single value
-const SITE_VERSION = '4.1'; // Update this number to cache bust all resources
+const SITE_VERSION = '4.2'; // Update this number to cache bust all resources
